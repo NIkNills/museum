5bf379ccb49bc2d7dea4287694a4046e0d9c0dc3
Nik Nills <niknills94@gmail.com>
2021-12-01 21:01:21 +0300

    feat: add swiper in welcome and video-journey sections

--- pages/main/main.js
+++ pages/main/main.js
@@ -23,3 +23,108 @@ seniorBtnDec.addEventListener('click', function(){
 seniorBtnInc.addEventListener('click', () => seniorInput.value++);
 
 buyNowBtn.addEventListener('click', () => console.log(`Selected tickets type: ${tickets.value}; number of basic tickets: ${basicInput.value}; number of senior tickets: ${seniorInput.value}`));
+
+const welcomeSwiper = new Swiper('.welcome-swiper', {
+    speed: 1000,
+    direction: 'horizontal',
+    loop: true,
+    slideClass: 'welcome-swiper__slide',
+    wrapperClass: 'welcome-swiper__wrapper',
+
+    autoplay: {
+      delay: 5000,
+    },
+  
+    pagination: {
+      el: '.welcome-swiper__pagination',
+      type: 'bullets',
+      bulletClass: 'welcome-swiper__pagination-bullet',
+      bulletActiveClass: 'welcome-swiper__pagination-bullet-active',
+      modifierClass: 'welcome-swiper__pagination-',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.welcome-swiper__btn-next',
+      prevEl: '.welcome-swiper__btn-prev',
+    },
+});
+
+const pages = document.querySelector('.welcome-swiper__pages');
+const totalBullets = document.querySelectorAll('.welcome-swiper__pagination-bullet').length;
+
+const currentPage = document.createElement('span');
+currentPage.className = 'welcome-swiper__page';
+currentPage.innerHTML = ++welcomeSwiper.realIndex;
+pages.prepend(currentPage);
+
+const totalPage = document.createElement('span');
+totalPage.className = 'welcome-swiper__page';
+totalPage.innerHTML = totalBullets;
+pages.append(totalPage);
+
+welcomeSwiper.on('transitionStart', () => {
+  currentPage.innerHTML = ++welcomeSwiper.realIndex;
+});
+
+const videoSwiperXL = new Swiper('.video-swiper_xl', {
+  speed: 1000,
+  direction: 'horizontal',
+  slideClass: 'video-swiper__slide',
+  wrapperClass: 'video-swiper__wrapper',
+  loop: true,
+
+  lazy: {
+    loadPrevNext: true,
+  },
+
+  pagination: {
+    el: '.video-swiper__pagination',
+    type: 'bullets',
+    bulletClass: 'video-swiper__pagination-bullet',
+    bulletActiveClass: 'video-swiper__pagination-bullet-active',
+    modifierClass: 'video-swiper__pagination-',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.video-swiper__btn-next',
+    prevEl: '.video-swiper__btn-prev',
+  },
+});
+
+const videoSwiperS = new Swiper('.video-swiper_s', {
+  speed: 900,
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  slidesPerGroup: 2,
+  spaceBetween: 42,
+  slideClass: 'video-swiper__slide',
+  wrapperClass: 'video-swiper__wrapper',
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+
+  lazy: {
+    loadPrevNext: true,
+  },
+ 
+  pagination: {
+    el: '.video-swiper__pagination',
+    type: 'bullets',
+    bulletClass: 'video-swiper__pagination-bullet',
+    bulletActiveClass: 'video-swiper__pagination-bullet-active',
+    modifierClass: 'video-swiper__pagination-',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.video-swiper__btn-next',
+    prevEl: '.video-swiper__btn-prev',
+  },
+});
